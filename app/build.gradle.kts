8ed418b9ecfcf00171ee615d4b123faf1bc341a5
ayushpant1 <ayushpant07@gmail.com>
2025-01-09 12:47:49 +0530

    basic setup to run app and add splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.ksp)
     alias(libs.plugins.room)
 }
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.retro.retroapp"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -80,4 +80,5 @@ dependencies {
 
     //additional dependencies
     implementation(libs.androidx.multidex)
+    implementation(libs.androidx.core.splashscreen)
 }
--- app/src/main/java/com/retro/retroapp/ui/screens/activities/MainActivity.kt
+++ app/src/main/java/com/retro/retroapp/ui/screens/activities/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,14 +12,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.retro.retroapp.ui.theme.RetroAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val splashViewModel: MainActivityViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        installSplashScreen().apply {
+            setKeepOnScreenCondition { splashViewModel.isLoading.value }
+        }
         setContent {
             RetroAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
